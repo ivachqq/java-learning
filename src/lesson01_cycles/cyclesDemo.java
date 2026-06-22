985fd87f3bb79d1ac2e0cfaa3b2269539e97abbe
ivachqq <ivanpes530@gmail.com>
2026-06-22 13:48:29 +0300

    organize lessons structure

---
+++ src/lesson01_cycles/cyclesDemo.java
@@ -0,0 +1,15 @@
+package lesson01_cycles;
+
+
+public class cyclesDemo {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
